1b1d570ded5caaefedd64d98b9085e77dc633bdb
arijit-ship <dasarijit1st@gmail.com>
2024-12-18 17:38:18 +0900

    hamiltonian expession fixed

--- src/observable.py
+++ src/observable.py
@@ -7,11 +7,11 @@ def constructObservable(nqubits: int, definition: str, coefficient: Dict[str, fl
     """
     Constructs the target observable (Hamiltonian).
 
-    definition: 'custom', 'ising', 'xy_model-xz-y', or 'heisenberg'.
-    'custom' is created using definition: 'xy_model-xz-y' which is an XY-model Hamiltonian.
+    definition: 'custom', 'ising', 'xy_model-xz-z', or 'heisenberg'.
+    'custom' is created using definition: 'xy_model-xz-z' which is an XY-model Hamiltonian.
 
     ### WARNING! Coefficients are applicable only for def: 'custom', and are overwritten
-    ### if def is specified as an in-built: 'ising', 'xy_model-xz-y', or 'heisenberg'.
+    ### if def is specified as an in-built: 'ising', 'xy_model-xz-z', or 'heisenberg'.
 
     Args:
         nqubits (int): Number of qubits in the system.
@@ -47,27 +47,35 @@ def constructObservable(nqubits: int, definition: str, coefficient: Dict[str, fl
         )
 
     if definition.lower() == "custom":
+
         target_observable = create_xy_hamiltonian(nqubits=nqubits, cn=observable_cn, bn=observable_bn, r=observable_r)
+
     elif definition.lower() == "ising":
+
         observable_cn = [0.5 for _ in range(nqubits - 1)]
         observable_bn = [1.0 for _ in range(nqubits)]
         observable_r = 1.0
 
         target_observable = create_xy_hamiltonian(nqubits=nqubits, cn=observable_cn, bn=observable_bn, r=observable_r)
+
     elif definition.lower() == "xy_model_xz-y":
+
         observable_cn = [1.0 for _ in range(nqubits - 1)]
         observable_bn = [1.0 for _ in range(nqubits)]
         observable_r = 1.0
 
         target_observable = create_xy_hamiltonian(nqubits=nqubits, cn=observable_cn, bn=observable_bn, r=observable_r)
+
     elif definition.lower() == "heisenberg":
+
         observable_cn = [1.0 for _ in range(nqubits - 1)]
 
         target_observable = create_heisenberg_hamiltonian(nqubits=nqubits, cn=observable_cn)
+
     else:
         raise ValueError(
             f"Invalid target definition: {definition}. "
-            f"Valid definitions are: 'custom', 'ising', 'xy_model-xz-y', or 'heisenberg'."
+            f"Valid definitions are: 'custom', 'ising', 'xy_model-xz-z', or 'heisenberg'."
         )
 
     return target_observable
